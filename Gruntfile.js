8da6f6abd57604abe548cc92e81f50a08274a723
Sebastian Seilund <sebastian@seilund.com>
2013-10-27 00:58:43 -0700

    added initial files

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        billy_builder: {
+            title: 'svg-flag-icons'
+        }
+    });
+
+    grunt.loadNpmTasks('billy-builder');
+};
